60383f3822313b69ee7377489fe152d294a2cc0f
luowei2014 <342549258@qq.com>
2020-02-08 17:04:08 +0800

    new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,24 @@
 	<div class="hello" >
 		<h1 style="color: red;">{{msg}}</h1>
 		<h3>实时疫情数据</h3>
+		<div id="echartsDemo2" style="width: 100%;height: 500px;"></div>
+		<span style="margin-top:20px;">选择数据量：</span>
+		<select v-model="datalength" @change="dataLengthChange($event)" style="width: 200px;margin-top:20px;">
+			<option value="500" selected:disabled>所有城市</option>
+			<option value="10">确诊前10城市</option>
+			<option value="20">确诊前20城市</option>
+			<option value="40">确诊前40城市</option>
+			<option value="60">确诊前60城市</option>
+			<option value="80">确诊前80城市</option>
+			<option value="100">确诊前100城市</option>
+		</select>
+		<div id="echartsDemo" style="width: 100%;height: 500px;"></div>
+		
 		<table style="border: 1px solid silver;padding: 10px;width: 100%;height: 400px;overflow: scroll;">
 			<tr class="head-tr">
-				<th class="head-th" v-for="itemhv in headList" v-bind:key="itemhv">{{itemhv}}</th>
+				<th class="head-th" v-for="(itemhv,index) in headList" v-bind:key="index">{{itemhv}}</th>
 			</tr>
-			<tr v-for="itemv in virusDdata" v-bind:key="itemv">
+			<tr v-for="(itemv,index) in virusDdata" v-bind:key="index">
 				<th class="cyp" style="background-color: chocolate;">{{itemv.name}}</th>
 				<th class="cyp" style="background-color: burlywood;">{{itemv.province}}</th>
 				<th>{{itemv.confirm}}</th>
@@ -18,9 +31,9 @@
 		<h2>热门迁入城市：</h2>
 		<table style="border: 1px solid silver;padding: 10px;width: 100%;">
 			<tr class="head-tr">
-				<th class="head-th" v-for="itemhin in headList2" v-bind:key="itemhin">{{itemhin}}</th>
+				<th class="head-th" v-for="(itemhin,index) in headList2" v-bind:key="index">{{itemhin}}</th>
 			</tr>
-			<tr v-for="itemin in inoutData" v-bind:key="itemin">
+			<tr v-for="(itemin,index) in inoutData" v-bind:key="index">
 				<th class="city cyp" style="background-color: chocolate;">{{itemin.cityName}}</th>
 				<th class="province cyp" style="background-color: burlywood;">{{itemin.provinceName}}</th>
 				<th v-if="itemin.value >= 1" style="background-color: red;color: white;font-weight: bolder;">{{itemin.value}}</th>
@@ -32,9 +45,9 @@
 		<h2>热门迁出城市：</h2>
 		<table style="border: 1px solid silver;padding: 10px;width: 100%;">
 			<tr class="head-tr">
-				<th class="head-th" v-for="itemout in headList3" v-bind:key="itemout">{{itemout}}</th>
+				<th class="head-th" v-for="(itemhout,index) in headList3" v-bind:key="index">{{itemhout}}</th>
 			</tr>
-			<tr v-for="itemout in outData" v-bind:key="itemout">
+			<tr v-for="(itemout,index) in outData" v-bind:key="index">
 				<th class="city cyp" style="background-color: chocolate;">{{itemout.cityName}}</th>
 				<th class="province cyp" style="background-color: burlywood;">{{itemout.provinceName}}</th>
 				<th v-if="itemout.value >= 1" style="background-color: red;color: white;font-weight: bolder;">{{itemout.value}}</th>
@@ -47,6 +60,7 @@
 </template>
 
 <script>
+	import '../../node_modules/echarts/map/js/china.js'
 export default {
 	data() {
 		return {
@@ -55,7 +69,8 @@ export default {
 			headList3: ["城市","省份","迁出人口比例(%)"],
 			inoutData: [],
 			outData: [],
-			virusDdata:[]
+			virusDdata:[],
+			datalength:200
 		}
 	},
 	methods: {
@@ -82,7 +97,7 @@ export default {
 				]
 			}).then(function(r){
 					that.inoutData=r.data.data;
-					window.console.log(that.inoutData);
+					
 				}).catch(err => window.console.log(err));
 				
 				
@@ -128,10 +143,127 @@ export default {
 						]
 					}).then(function(r){
 							that.virusDdata=r.data.data;
+							var keyarray=new Array;
+							var valarray=new Array;
+							for(var i=0;i<r.data.data.length;i++){
+								keyarray[i]=r.data.data[i].name;
+								valarray[i]=r.data.data[i].confirm;
+							}
+							window.console.log(r.data.data);
+							that.drawBar(keyarray,valarray);
 						}).catch(err => window.console.log(err));
 						
+						//疫情实时地图数据
+						this.$http({
+							method: 'post',
+							url: 'virus/map/list',
+							headers: {
+								'Content-type': 'application/x-www-form-urlencoded'
+							},
+							data: {}
+
+						}).then(function(r){
+
+								that.drawMap(r.data.data);
+							}).catch(err => window.console.log(err));
 						
-		}
+						
+		},
+		drawBar (keyarray,valarray) {
+		      let echartsBarDemo = this.$echarts.init(document.getElementById('echartsDemo'))
+		      let option = {
+		              title: {
+		                text: '实时疫情柱状图'
+		              },
+		              tooltip: {},
+		              legend: {
+		                data: ['确诊数']
+		              },
+		              xAxis: {
+		                data: keyarray
+		              },
+		              yAxis: {},
+		              series: [{
+		                name: '确诊',
+		                type: 'bar',
+		                data: valarray
+		              }]
+		            }
+		            echartsBarDemo.setOption(option)
+		      
+
+		    },
+			drawMap(dataList){
+				window.console.log(dataList);
+				let echartsMapDemo = this.$echarts.init(document.getElementById('echartsDemo2'))
+				
+						echartsMapDemo.setOption({
+							backgroundColor: "#000000",
+								tooltip: {}, // 鼠标移到图里面的浮动提示框
+								dataRange: {
+									show: false,
+									min: 0,
+									max: 1000,
+									text: ['High', 'Low'],
+									realtime: true,
+									calculable: true,
+									color: ['#870002', '#360A0F', '#662E44','#274744']
+								},
+								geo: { // 这个是重点配置区
+									map: 'china', // 表示中国地图
+									roam: true,
+									label: {
+										normal: {
+											show: true, // 是否显示对应地名
+											textStyle: {
+												color: 'white'
+											}
+										}
+									},
+									itemStyle: {
+										normal: {
+											borderColor: '#3E101E'
+										},
+										emphasis: {
+											areaColor: null,
+											shadowOffsetX: 0,
+											shadowOffsetY: 0,
+											shadowBlur: 20,
+											borderWidth: 0,
+											shadowColor: '#3E101E'
+										}
+									}
+								},
+								series: [{
+										type: 'scatter',
+										coordinateSystem: 'geo' // 对应上方配置
+									},
+									{
+										name: '确诊人数', // 浮动框的标题
+										type: 'map',
+										geoIndex: 0,
+										data: dataList
+									}
+								]
+						})
+			},
+			dataLengthChange(event){
+				var that=this;
+
+				window.console.log(that.datalength);
+				
+				var keyarray=new Array;
+				var valarray=new Array;
+				for(var i=0;i<that.virusDdata.length;i++){
+					if(i<=that.datalength){
+						keyarray[i]=that.virusDdata[i].name;
+						valarray[i]=that.virusDdata[i].confirm;
+					}
+				}
+				that.drawBar(keyarray,valarray);
+			}
+		
+		
 	},
 	mounted() {
 		this.getVirusData();
@@ -167,3 +299,7 @@ a {
 	color: #42b983;
 }
 </style>
+
+
+
+
